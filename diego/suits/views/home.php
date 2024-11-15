341224a0ce4b517dc5f61ab90495310dc7fe93d6
Axelito70 <depredadorr70@gmail.com>
2024-11-14 23:39:14 -0600

    ProyectoInterfas

---
+++ diego/suits/views/home.php
@@ -0,0 +1,7 @@
+<div class="container mt-5">
+    <div class="row justify-content-center bg-card">
+        <div class="col-10 text-center mt-3 tienda-container">
+            <h2>Mi tienda</h2>
+        </div>
+    </div>
+</div>
